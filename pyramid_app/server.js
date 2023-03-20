c69832d6461cb0a5a73739ff7b180b719c61c5af
tiffanysimione <simionetiffany1@gmail.com>
2023-03-19 20:29:34 -0400

    pushing

--- pyramid_app/server.js
+++ pyramid_app/server.js
@@ -2,12 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const ThreeSeed = require('./models/two.js');
-const expressLayouts = require('express-ejs-layouts');
 const app = express()
-
-
-app.use(expressLayouts);
-app.set('view engine', 'ejs')
 
 
 app.use(express.urlencoded({extended: true}))
@@ -30,9 +25,6 @@ app.get('/ones/pyramid', (req, res) => {
         res.render('pyramid.ejs', { allThrees: allThrees });
     });
 });
-
-app.use('/', require('./i'));
-app.use('/user', require('./user'));
 
 
 
@@ -65,7 +57,6 @@ app.get('/ones/:id/edit', (req,res)=>{
 // })
 
 
-
 app.put('/ones/:id', (req, res) => {
     ThreeSeed.findByIdAndUpdate(req.params.id, req.body, {new: true}).then((updatedThreeSeed) => {
         res.redirect('/ones')
@@ -86,10 +77,6 @@ app.delete('/ones/:id', (req, res) => {
   })
   
 
-// mongoose.connect(db, { userNewUrlParser: true})
-// .then(()=>console.log('MongoDb Connected'))
-// .catch(err => console.log(err));
-
 mongoose.connect('mongodb://localhost:27017/basiccrud').then(() => {
    console.log('conneciton with mongo established')
 })
@@ -103,5 +90,3 @@ const PORT =process.env.PORT ||3000;
 app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
  });
-
- 
